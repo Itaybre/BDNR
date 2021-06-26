f7a097cde32dadd3e2f52eefeaa88a96935c8b8f
Mateo Kruk <mkrukuy@gmail.com>
2021-06-26 18:54:09 -0300

    UI

--- feed/index.js
+++ feed/index.js
@@ -1,16 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("config");
+const cors = require("cors");
+const feedRouter = require("./routers/feed.route");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const config = require('config')
-const feedRouter = require('./routers/feed.route')
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/", feedRouter);
 
-app.use(bodyParser.json())
-app.use('/', feedRouter)
-
-const port = config.has('port') ? config.get('port') : 5008
+const port = config.has("port") ? config.get("port") : 5008;
 
 app.listen(Number(port), () => {
-  console.log(`Feed Server started on port ${port}`)
-})
+  console.log(`Feed Server started on port ${port}`);
+});
