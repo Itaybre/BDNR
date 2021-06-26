bd632e6e74fb3bf0355670a164faa756e3ef9341
itay <itay.brenner@mercadolibre.com>
2021-06-26 03:44:36 -0300

    Run standard

--- feed/controllers/feed.controller.js
+++ feed/controllers/feed.controller.js
@@ -3,49 +3,49 @@ const config = require('config')
 const redis = new Redis(config.get('redis'))
 
 exports.getAllFeeds = async (req, res) => {
-    return res.status(400).send('Please request a feed for a single user')
+  return res.status(400).send('Please request a feed for a single user')
 }
 
 exports.getFeed = async (req, res) => {
-    let userId = req.params.user_id
-    if ( !(await redis.exists(userId)) ) {
-        return res.status(404).send('User does not have a feed')
-    }
+  const userId = req.params.user_id
+  if (!(await redis.exists(userId))) {
+    return res.status(404).send('User does not have a feed')
+  }
 
-    res.send(JSON.parse(await redis.get(userId)))
+  res.send(JSON.parse(await redis.get(userId)))
 }
 
 exports.createFeed = async (req, res) => {
-    let userId = req.body.userId
-    await redis.set(userId, JSON.stringify(req.body))
+  const userId = req.body.userId
+  await redis.set(userId, JSON.stringify(req.body))
 
-    res.send(req.body)
+  res.send(req.body)
 }
 
 exports.updateFeed = async (req, res) => {
-    let userId = req.params.user_id
-    if (userId !== req.body.userId) {
-        return res.status(404).send('Body should not contain a different userId')
-    }
-    if ( !(await redis.exists(userId)) ) {
-        return res.status(404).send('User does not have a feed')
-    }
+  const userId = req.params.user_id
+  if (userId !== req.body.userId) {
+    return res.status(404).send('Body should not contain a different userId')
+  }
+  if (!(await redis.exists(userId))) {
+    return res.status(404).send('User does not have a feed')
+  }
 
-    let originalFeed = JSON.parse(await redis.get(userId))
-    for (const key in req.body) {
-        originalFeed[key] = req.body[key]
-    }
+  const originalFeed = JSON.parse(await redis.get(userId))
+  for (const key in req.body) {
+    originalFeed[key] = req.body[key]
+  }
 
-    await redis.set(userId, JSON.stringify(originalFeed))
-    res.send(originalFeed)
+  await redis.set(userId, JSON.stringify(originalFeed))
+  res.send(originalFeed)
 }
 
 exports.destroyFeed = async (req, res) => {
-    let userId = req.params.user_id
-    if ( !(await redis.exists(userId)) ) {
-        return res.status(404).send('User does not have a feed')
-    }
+  const userId = req.params.user_id
+  if (!(await redis.exists(userId))) {
+    return res.status(404).send('User does not have a feed')
+  }
 
-    await redis.del(userId)
-    res.send('Deleted successfully')
+  await redis.del(userId)
+  res.send('Deleted successfully')
 }
